08780d1ccc978a34d1b56103804f1172303b6695
alexmeys <47882875+alexmeys@users.noreply.github.com>
2019-02-22 14:36:37 +0100

    Create removeexif.go

---
+++ removeexif.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+    "os"
+	"io/ioutil"
+	"log"
+	"image"
+	"image/jpeg"
+)
+
+func main() {
+
+    // read current directory and subdirectory
+    bestanden, err := ioutil.ReadDir(".")
+    if err != nil {
+        log.Fatal("Failed, %s\n", err)
+    }
+	for _, file := range bestanden {	
+		bestand, err := os.Open(file.Name())
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer bestand.Close()
+
+		img, _, err := image.Decode(bestand)
+		if err != nil {
+			log.Fatal("Failed, %s\n", err)
+		}
+
+		outfile, err := os.Create(file.Name())
+		if err != nil {
+			log.Fatal("Failed, %s\n", err)
+		}
+		defer outfile.Close()
+
+		if err := jpeg.Encode(outfile, img, nil); err != nil {
+			log.Fatal("Failed, %s\n", err)
+		}
+	}
+}
